fdf18fa0ec1484bedf50c1b2e0d7fd4cf066546a
namedpython <namedpython+dev@gmail.com>
2023-02-01 01:55:43 +0900

    add: namingConvention version

---
+++ apps/hasura-default/codegen-role-naming.ts
@@ -0,0 +1,46 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+import { Types } from "@graphql-codegen/plugin-helpers/typings";
+
+// 環境変数からエンドポイントやシークレットを注入
+const hasuraSchema = (
+  role = "admin",
+  endpoint = process.env.HASURA_GRAPHQL_ENDPOINT,
+  secret = process.env.HASURA_GRAPHQL_ADMIN_SECRET
+): Types.UrlSchemaWithOptions => {
+  const hasuraSchemaEndpoint = `${endpoint}/v1/graphql`;
+  const schemaObj: Types.UrlSchemaWithOptions = {};
+
+  console.log(`-- read schema as ${role} from: ${hasuraSchemaEndpoint}`);
+
+  schemaObj[hasuraSchemaEndpoint] = {
+    headers: {
+      "x-hasura-admin-secret": secret || "",
+      "x-hasura-role": role,
+    },
+  };
+
+  return schemaObj;
+};
+
+const config: CodegenConfig = {
+  config: {
+    skipTypename: false,
+    withHooks: true,
+    withHOC: false,
+    withComponent: false,
+    gqlImport: "@urql/core#gql",
+    namingConvention: {
+      enumValues: 'change-case-all#pascalCase',
+      typeNames: 'change-case-all#pascalCase',
+      transformUnderscore: true,
+    },
+  },
+  generates: {
+    "./src/graphql/types-naming.ts": {
+      plugins: ["typescript"],
+      schema: [hasuraSchema()],
+    },
+  },
+};
+
+export default config;
